3a522f657a6021b7e89750b9df9d3cdb6b5a3134
leesinvo <84228165+leesinvo@users.noreply.github.com>
2025-05-22 02:22:46 +0100

    Add app/src/main/java/com/example/truckdelivery/ui/theme/Color.kt 1747876964651

---
+++ app/src/main/java/com/example/truckdelivery/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package com.example.truckdelivery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val Blue40 = Color(0xFF1976D2)
+val BlueGrey40 = Color(0xFF546E7A)
+val LightBlue40 = Color(0xFF03A9F4)
+val LightBackground = Color(0xFFFAFAFA)
+val LightSurface = Color(0xFFFFFFFF)
+
+// Dark Theme Colors
+val Blue80 = Color(0xFF64B5F6)
+val BlueGrey80 = Color(0xFF90A4AE)
+val LightBlue80 = Color(0xFF81D4FA)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+
+// Common Colors
+val ErrorRed = Color(0xFFB00020)
+val SuccessGreen = Color(0xFF4CAF50)
+val WarningYellow = Color(0xFFFFC107)
+val InfoBlue = Color(0xFF2196F3)
+
+// Map Marker Colors
+val TruckMarkerColor = Color(0xFF1976D2)
+val ShopMarkerColor = Color(0xFF4CAF50)
+val DeliveryRouteColor = Color(0xFF2196F3)
+
+// Status Colors
+val PendingColor = Color(0xFFFFA000)
+val InProgressColor = Color(0xFF1976D2)
+val CompletedColor = Color(0xFF43A047)
+val CancelledColor = Color(0xFFD32F2F)
+
+// Button Colors
+val PrimaryButtonColor = Blue40
+val SecondaryButtonColor = BlueGrey40
+val DisabledButtonColor = Color(0xFFBDBDBD)
